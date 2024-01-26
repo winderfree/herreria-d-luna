0140bdf97c7b129c5c37827ed9806997101a6cee
Juan Casado Luna <juans.casado@gmail.com>
2024-01-26 11:29:50 -0400

    primer commit

---
+++ js.js
@@ -0,0 +1,12 @@
+console.log("Hola!");
+
+const foto_html = document.querySelector("#html");
+const foto_css3 = document.querySelector("#css");
+const foto_javascript = document.querySelector("#javascript");
+const foto_python = document.querySelector("#python");
+
+foto_html.src = "imagenes/fotos-tech-inow/pinturas.png";
+foto_css3.src = "imagenes/fotos-tech-inow/herreria.png";
+foto_javascript.src = "imagenes/fotos-tech-inow/sordadura.png";
+// foto_python.src = "imagenes/fotos-tech-inow/Python.png";
+
